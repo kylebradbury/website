c7b6efe6ac2f417e13599ee5c935d479832002a6
kylebradbury <kbradbury@gmail.com>
2018-07-04 12:45:55 -0400

    Small revision before major revision

---
+++ data_extraction/extract_data.py
@@ -0,0 +1,196 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+import pyperclip
+
+def convert_courses_to_table(df):
+    # Setup table headers
+    html = '<table class="table">\n\t<thead>\n\t<tr>\n'
+    
+    # Add table headers
+    headers = ['Instructor', 'Title', 'Designation', 'University', 'Year', '']
+    for head in headers:
+        html += '\t\t<th>' + head + '</th>\n'
+    
+    # Close headers
+    html += '\t</tr>\n\t</thead>\n'
+    
+    # Begin rows
+    html += '\t<tbody class="list">\n'
+    
+    # Add Rows
+    for index,doc in df.iterrows():
+        html += '\t\t<tr>\n'
+        html += '\t\t\t<td class="author">' + doc['Instructor Last'] + '</td>\n'            
+        html += '\t\t\t<td class="title"><span class="booktitle"><a href=\'' + doc['Website'] + '\' target="_blank">' + doc['Course Name'] + '</a></span</td>\n'        
+        html += '\t\t\t<td class="designation">' + doc['Course Designation'] + '</span></td>\n'        
+        html += '\t\t\t<td class="university">' + doc['University'] + '</span></td>\n'
+        html += '\t\t\t<td class="year">' + str(doc['Year']) + '</td>\n'
+        if doc['Starred']=='Yes':
+            html += '\t\t\t<td class="starred"><i class="fa fa-star favorite" aria-hidden="true"></i></td>\n'
+        else:
+            html += '\t\t\t<td class="starred"></td>\n'
+        html += '\t\t</tr>\n'
+    
+    # End Rows and table
+    html += '\t</tbody>\n</table>\n'
+    
+    return html
+
+def convert_references_to_table(df):
+    # Setup table headers
+    html = '<table class="table">\n\t<thead>\n\t<tr>\n'
+    
+    # Add table headers
+    headers = ['Author', 'Name', 'Topic', 'Year', '']
+    for head in headers:
+        html += '\t\t<th>' + head + '</th>\n'
+    
+    # Close headers
+    html += '\t</tr>\n\t</thead>\n'
+    
+    # Begin rows
+    html += '\t<tbody class="list">\n'
+    
+    # Add Rows
+    for index,doc in df.iterrows():
+        html += '\t\t<tr>\n'
+        html += '\t\t\t<td class="author">' + doc['Author'] + '</td>\n'
+            
+        if doc['Free']=='Yes':
+            html += '\t\t\t<td class="title"><span class="booktitle"><a href=\'' + doc['Link'] + '\' target="_blank">' + doc['Name'] + '</a></span></td>\n'
+        else:
+            html += '\t\t\t<td class="title">' + doc['Name'] + '</td>\n'
+        
+        if pd.isnull(doc['Subcategory']):
+            html += '\t\t\t<td class="category">' + doc['Category'] + '</span></td>\n'
+        else:
+            html += '\t\t\t<td class="category">' + doc['Category'] + ' - ' + str(doc['Subcategory']) + '</td>\n'
+            
+        html += '\t\t\t<td class="year">' + str(doc['Year']) + '</td>\n'
+        
+        if doc['Starred']=='Yes':
+            html += '\t\t\t<td class="starred"><i class="fa fa-star favorite" aria-hidden="true"></i></td>\n'
+        else:
+            html += '\t\t\t<td class="starred"></td>\n'
+        
+        html += '\t\t</tr>\n'
+    
+    # End Rows and table
+    html += '\t</tbody>\n</table>\n'
+    
+    return html
+
+def convert_tools_to_table(df):
+    # Setup table headers
+    html = '<table class="table">\n\t<thead>\n\t<tr>\n'
+    
+    # Add table headers
+    headers = ['Name', 'Topic', 'Description']
+    for head in headers:
+        html += '\t\t<th>' + head + '</th>\n'
+    
+    # Close headers
+    html += '\t</tr>\n\t</thead>\n'
+    
+    # Begin rows
+    html += '\t<tbody class="list">\n'
+    
+    # Add Rows
+    for index,doc in df.iterrows():
+        html += '\t\t<tr>\n'          
+        html += '\t\t\t<td class="name"><span class="booktitle"><a href=\'' + doc['Link'] + '\' target="_blank">' + doc['Name'] + '</a></span</td>\n'        
+        html += '\t\t\t<td class="topic">' + doc['Topic'] + '</td>\n'  
+        html += '\t\t\t<td class="description">' + doc['Description'] + '</span></td>\n'        
+        html += '\t\t</tr>\n'
+    
+    # End Rows and table
+    html += '\t</tbody>\n</table>\n'
+    
+    return html
+
+def convert_videos_to_table(df):
+    # Setup table headers
+    html = '<table class="table">\n\t<thead>\n\t<tr>\n'
+    
+    # Add table headers
+    headers = ['Author', 'Organization', 'Name', 'Description']
+    for head in headers:
+        html += '\t\t<th>' + head + '</th>\n'
+    
+    # Close headers
+    html += '\t</tr>\n\t</thead>\n'
+    
+    # Begin rows
+    html += '\t<tbody class="list">\n'
+    
+    # Add Rows
+    for index,doc in df.iterrows():
+        html += '\t\t<tr>\n'
+        html += '\t\t\t<td class="author">' + doc['Author'] + '</td>\n'
+        html += '\t\t\t<td class="organization">' + doc['Organization'] + '</td>\n'            
+        html += '\t\t\t<td class="name"><span class="booktitle"><a href=\'' + doc['Link'] + '\' target="_blank">' + doc['Name'] + '</a></span</td>\n'        
+        html += '\t\t\t<td class="description">' + doc['Description'] + '</span></td>\n'        
+        html += '\t\t</tr>\n'
+    
+    # End Rows and table
+    html += '\t</tbody>\n</table>\n'
+    
+    return html
+
+def convert_data_to_table(df):
+    # Setup table headers
+    html = '<table class="table">\n\t<thead>\n\t<tr>\n'
+    
+    # Add table headers
+    headers = ['Name', 'Description']
+    for head in headers:
+        html += '\t\t<th>' + head + '</th>\n'
+    
+    # Close headers
+    html += '\t</tr>\n\t</thead>\n'
+    
+    # Begin rows
+    html += '\t<tbody class="list">\n'
+    
+    # Add Rows
+    for index,doc in df.iterrows():
+        html += '\t\t<tr>\n'           
+        html += '\t\t\t<td class="name"><span class="booktitle"><a href=\'' + doc['Link'] + '\' target="_blank">' + doc['Name'] + '</a></span</td>\n'        
+        html += '\t\t\t<td class="description">' + doc['Description'] + '</span></td>\n'        
+        html += '\t\t</tr>\n'
+    
+    # End Rows and table
+    html += '\t</tbody>\n</table>\n'
+    
+    return html
+
+filename = 'Data Science Resources.xlsx'
+
+courses = pd.read_excel(filename, sheetname="Courses")
+html_courses = convert_courses_to_table(courses)
+
+references = pd.read_excel(filename, sheetname="References")
+html_references = convert_references_to_table(references)
+
+tools = pd.read_excel(filename, sheetname="Tools")
+html_tools = convert_tools_to_table(tools)
+
+videos = pd.read_excel(filename, sheetname="Videos")
+html_videos = convert_videos_to_table(videos)
+
+data = pd.read_excel(filename, sheetname="Data")
+html_data = convert_data_to_table(data)
+
+f = open('references.txt','w')
+f.write('\n####### COURSES ######\n\n')
+f.write(html_courses)
+f.write('\n####### REFERENCES ######\n\n')
+f.write(html_references)
+f.write('\n####### TOOLS ######\n\n')
+f.write(html_tools)
+f.write('\n####### VIDEOS ######\n\n')
+f.write(html_videos)
+f.write('\n####### DATA ######\n\n')
+f.write(html_data)
+f.close()
